ba5e218fdee236515a9fcaa3299528341314f545
GEORGIAN86 <sumitgeorgian3986@gmail.com>
2024-08-17 18:37:21 +0530

    deploy1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist' // Ensure this matches the expected output directory
-  }
+    outDir: 'dist', // Make sure this is correct
+  },
 });
