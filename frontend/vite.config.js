2c802c71d36112d55039bfb675d0d95c06587ea4
GEORGIAN86 <sumitgeorgian3986@gmail.com>
2024-08-17 19:48:05 +0530

    deploy1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Make sure this is correct
+    outDir: 'dist', // Ensure this matches the directory specified in vercel.json
   },
 });
